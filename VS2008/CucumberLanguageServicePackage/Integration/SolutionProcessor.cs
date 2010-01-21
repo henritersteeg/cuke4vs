f0ba0bb8ee47d11350db5f4cb4d99702c6f7e507
Henri ter Steeg <henri@ter-steeg.com>
2010-01-21 16:04:26 +0100

    Processing the solution in search for step definitions, should not block opening the first feature file.

--- VS2008/CucumberLanguageServicePackage/Integration/SolutionProcessor.cs
+++ VS2008/CucumberLanguageServicePackage/Integration/SolutionProcessor.cs
@@ -8,11 +8,18 @@ using ProductivityPackage;
 
 namespace CucumberLanguageServices.Integration
 {
-    public class SolutionProcessor
+    public class SolutionProcessor 
     {
         public StepProvider StepProvider { get; set; }
         public IVsSolution Solution { get; set; }
+
         public void Process()
+        {
+            var thread = new System.Threading.Thread(Run);
+            thread.Start();
+        }
+
+        private void Run()
         {
             // We don't use Linq here, since this fails for some mystical reason... 
             foreach (var node in new HierarchyNodeIterator(Solution))
